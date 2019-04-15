80506b593ff46cdcab8739d62ffc06c33f1d2c16
Surjit Konjengbam <ssing01@Surjits-MBP.home>
2019-04-15 07:05:48 -0400

    travis test for condition

--- scripts/run_proc.sh
+++ scripts/run_proc.sh
@@ -3,7 +3,14 @@ set -ev
 
 echo "$CHANGED_FILES"
 
-echo "testing starts"
+echo "test 1 starts"
+
+for fname in "$CHANGED_FILES"
+do
+  echo $fname
+done
+
+echo "testing 2 starts"
 
 if [ "${TRAVIS_PULL_REQUEST}" = "false" ]; then
   echo "test success"
