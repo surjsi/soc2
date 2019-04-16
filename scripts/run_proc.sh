5ddf822ddadc1d8e9b44a9161be4b07471c09d0f
Surjit Konjengbam <ssing01@Surjits-MBP.home>
2019-04-15 21:04:49 -0400

    test

--- scripts/run_proc.sh
+++ scripts/run_proc.sh
@@ -6,4 +6,23 @@ echo "$CHANGED_FILES"
 for fname in "$CHANGED_FILES"
 do
   echo "File name is : $fname"
+  if echo "$fname" | grep -q "procedures"`; then
+ # if [ `grep "procedure" $fname ` ]; then
+
+    for fle in `grep -l "^index:" $fname `
+    do
+      echo "File having index: $fle "
+
+      if [ `grep "^cron" $fle | wc -l` -eq 0 ]; then
+ #       indxVal=`grep "^index:" $fle| cut -d":" -f 2`
+ #       echo $indxVal
+ #       indxVal=${indxVal#*'"'}; indxVal=${s%'"'*}
+ #       echo "value after stripped quptes $indxVal"
+        echo "file has no cron $fle"
+      fi
+
+    done
+
+  fi
+
 done
