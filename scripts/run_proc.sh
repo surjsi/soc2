d7687fa0893bbf247983c6b945c300d66f90dced
Surjit Konjengbam <ssing01@Surjits-MBP.home>
2019-04-15 21:01:18 -0400

    test

--- scripts/run_proc.sh
+++ scripts/run_proc.sh
@@ -6,9 +6,4 @@ echo "$CHANGED_FILES"
 for fname in "$CHANGED_FILES"
 do
   echo "File name is : $fname"
-  if echo "$fname" | grep -q "procedures"`; then
-    echo "$fname is a proc"  
-  fi
-
 done
-
